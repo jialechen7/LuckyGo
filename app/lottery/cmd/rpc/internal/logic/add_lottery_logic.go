19581d8288a5786c254c8071f06dac7713c3904b
jialechen77 <1262968640@qq.com>
2025-04-05 20:07:52 +0800

    feat: optimize instant lottery using kafka

--- app/lottery/cmd/rpc/internal/logic/add_lottery_logic.go
+++ app/lottery/cmd/rpc/internal/logic/add_lottery_logic.go
@@ -2,12 +2,14 @@ package logic
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/jialechen7/go-lottery/app/lottery/model"
 	"github.com/jialechen7/go-lottery/common/constants"
 	"github.com/jialechen7/go-lottery/common/xerr"
 	"github.com/jinzhu/copier"
 	"github.com/pkg/errors"
 	"gorm.io/gorm"
+	"strconv"
 	"time"
 
 	"github.com/jialechen7/go-lottery/app/lottery/cmd/rpc/internal/svc"
@@ -15,6 +17,16 @@ import (
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
+
+type RedisPrize struct {
+	LotteryId        int64 `json:"lottery_id"`
+	Id               int64 `json:"id"`
+	Count            int64 `json:"count"`
+	Stock            int64 `json:"stock"`
+	Level            int64 `json:"level"`
+	LeftProbability  int64 `json:"left_probability"`
+	RightProbability int64 `json:"right_probability"`
+}
 
 type AddLotteryLogic struct {
 	ctx    context.Context
@@ -31,7 +43,6 @@ func NewAddLotteryLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddLot
 }
 
 func (l *AddLotteryLogic) AddLottery(in *pb.AddLotteryReq) (*pb.AddLotteryResp, error) {
-	logx.Error(in)
 	var lotteryId int64
 	err := l.svcCtx.TransactCtx(l.ctx, func(db *gorm.DB) error {
 		lottery := &model.Lottery{
@@ -58,6 +69,7 @@ func (l *AddLotteryLogic) AddLottery(in *pb.AddLotteryReq) (*pb.AddLotteryResp, 
 		}
 		lotteryId = lottery.Id
 
+		prizePoolKey := constants.InstantLotteryPrizePoolRedisKey + strconv.Itoa(int(lotteryId))
 		for _, pbPrize := range in.Prizes {
 			prize := new(model.Prize)
 			_ = copier.Copy(prize, pbPrize)
@@ -66,6 +78,20 @@ func (l *AddLotteryLogic) AddLottery(in *pb.AddLotteryReq) (*pb.AddLotteryResp, 
 			err := l.svcCtx.PrizeModel.Insert(l.ctx, db, prize)
 			if err != nil {
 				return errors.Wrapf(model.ErrCreatePrize, "AddLotteryLogic Insert Prize error: %v", err)
+			}
+			redisPrize := &RedisPrize{
+				LotteryId:        lotteryId,
+				Id:               prize.Id,
+				Count:            prize.Count,
+				Stock:            prize.Stock,
+				Level:            prize.Level,
+				LeftProbability:  int64(constants.ProbabilityMap[int(prize.Level)][0]),
+				RightProbability: int64(constants.ProbabilityMap[int(prize.Level)][1]),
+			}
+			jsonData, _ := json.Marshal(redisPrize)
+			err = l.svcCtx.RedisClient.HSet(l.ctx, prizePoolKey, strconv.FormatInt(redisPrize.Id, 10), jsonData).Err()
+			if err != nil {
+				return errors.Wrapf(xerr.NewErrCode(xerr.REDIS_ERROR), "AddLotteryLogic Redis HSet error: %v", err)
 			}
 		}
 
--- app/lottery/cmd/rpc/internal/svc/service_context.go
+++ app/lottery/cmd/rpc/internal/svc/service_context.go
@@ -3,6 +3,7 @@ package svc
 import (
 	"context"
 	"database/sql"
+	"github.com/IBM/sarama"
 	"github.com/SpectatorNan/gorm-zero/gormc/config/mysql"
 	"github.com/go-redsync/redsync/v4"
 	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
@@ -12,6 +13,7 @@ import (
 	goredislib "github.com/redis/go-redis/v9"
 	"github.com/zeromicro/go-zero/zrpc"
 	"gorm.io/gorm"
+	"time"
 )
 
 type ServiceContext struct {
@@ -21,7 +23,36 @@ type ServiceContext struct {
 	PrizeModel                model.PrizeModel
 	UsercenterRpc             usercenter.Usercenter
 	RedsyncClient             *redsync.Redsync
+	RedisClient               *goredislib.Client
+	Producer                  sarama.SyncProducer
 	TransactCtx               func(context.Context, func(db *gorm.DB) error, ...*sql.TxOptions) error
+}
+
+func getAddr(c config.Config) []string {
+	return c.Kafka.Host
+}
+
+// 创建 kafka 客户端连接的配置
+func getConf(c config.Config) *sarama.Config {
+	conf := sarama.NewConfig()
+	// 生产消息后是否需要通知生产者
+	// 同步模式会直接返回
+	// 异步模式会返回到Successes和Errors通道中
+	conf.Producer.Return.Successes = true
+	conf.Producer.Return.Errors = true
+	conf.Producer.RequiredAcks = sarama.WaitForAll
+	conf.Producer.Timeout = 5 * time.Second
+	conf.Producer.Retry.Max = 5
+	conf.Producer.Partitioner = sarama.NewRandomPartitioner
+	return conf
+}
+
+func newKafkaProducer(c config.Config) sarama.SyncProducer {
+	producer, err := sarama.NewSyncProducer(getAddr(c), getConf(c))
+	if err != nil {
+		panic(err)
+	}
+	return producer
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -34,15 +65,19 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		Password: c.Cache[0].Pass,
 	})
 	pool := goredis.NewPool(client)
-	return &ServiceContext{
+	svcCtx := &ServiceContext{
 		Config:                    c,
 		LotteryModel:              model.NewLotteryModel(db, c.Cache),
 		LotteryParticipationModel: model.NewLotteryParticipationModel(db, c.Cache),
 		PrizeModel:                model.NewPrizeModel(db, c.Cache),
 		UsercenterRpc:             usercenter.NewUsercenter(zrpc.MustNewClient(c.UsercenterRpcConf)),
 		RedsyncClient:             redsync.New(pool),
+		RedisClient:               client,
+		Producer:                  newKafkaProducer(c),
 		TransactCtx: func(ctx context.Context, fn func(db *gorm.DB) error, opts ...*sql.TxOptions) error {
 			return db.WithContext(ctx).Transaction(fn, opts...)
 		},
 	}
+
+	return svcCtx
 }
